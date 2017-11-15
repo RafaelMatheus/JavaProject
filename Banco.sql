55a326f25ac84950d80932dcf0d34bdeec5ead1e
Eduardo Diniz <33600213+Eduardogith@users.noreply.github.com>
2017-11-15 12:45:04 -0300

    Banco atualizado

---
+++ Banco.sql
@@ -0,0 +1,171 @@
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: Controle
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Agenda`
+--
+
+DROP TABLE IF EXISTS `Agenda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Agenda` (
+  `IDAgenda` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_Cliente` int(11) NOT NULL,
+  `Data_Cob` date NOT NULL,
+  `Data_Comp` date NOT NULL,
+  PRIMARY KEY (`IDAgenda`),
+  KEY `ID_Cliente` (`ID_Cliente`),
+  CONSTRAINT `Agenda_ibfk_1` FOREIGN KEY (`ID_Cliente`) REFERENCES `Cliente` (`IDCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Agenda`
+--
+
+LOCK TABLES `Agenda` WRITE;
+/*!40000 ALTER TABLE `Agenda` DISABLE KEYS */;
+INSERT INTO `Agenda` VALUES (1,1,'0000-00-00','0000-00-00'),(2,1,'0000-00-00','0000-00-00'),(3,1,'0000-00-00','0000-00-00'),(4,1,'1999-01-22','1999-01-20');
+/*!40000 ALTER TABLE `Agenda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Cliente`
+--
+
+DROP TABLE IF EXISTS `Cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cliente` (
+  `Nome` varchar(40) NOT NULL,
+  `DataNasc` date NOT NULL,
+  `Email` varchar(30) DEFAULT NULL,
+  `Cpf` varchar(10) NOT NULL,
+  `IDCliente` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`IDCliente`),
+  UNIQUE KEY `Cpf` (`Cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cliente`
+--
+
+LOCK TABLES `Cliente` WRITE;
+/*!40000 ALTER TABLE `Cliente` DISABLE KEYS */;
+INSERT INTO `Cliente` VALUES ('Eduardo','0000-00-00',NULL,'1999741',1),('Eduardo','0000-00-00',NULL,'1999731',3);
+/*!40000 ALTER TABLE `Cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Compras`
+--
+
+DROP TABLE IF EXISTS `Compras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Compras` (
+  `ID_Produto` int(11) NOT NULL,
+  `ID_Agenda` int(11) NOT NULL,
+  `QTD` int(11) NOT NULL,
+  `IDCompras` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`IDCompras`),
+  KEY `ID_Produto` (`ID_Produto`),
+  KEY `ID_Agenda` (`ID_Agenda`),
+  CONSTRAINT `Compras_ibfk_1` FOREIGN KEY (`ID_Produto`) REFERENCES `Produto` (`IDProduto`),
+  CONSTRAINT `Compras_ibfk_2` FOREIGN KEY (`ID_Agenda`) REFERENCES `Agenda` (`IDAgenda`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Compras`
+--
+
+LOCK TABLES `Compras` WRITE;
+/*!40000 ALTER TABLE `Compras` DISABLE KEYS */;
+INSERT INTO `Compras` VALUES (1,1,3,1),(1,1,4,2);
+/*!40000 ALTER TABLE `Compras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Contato`
+--
+
+DROP TABLE IF EXISTS `Contato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Contato` (
+  `IDContato` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_Cliente` int(11) NOT NULL,
+  `Rua` varchar(30) NOT NULL,
+  `Cidade` varchar(15) NOT NULL,
+  `Estado` char(2) NOT NULL,
+  `Numero` int(11) NOT NULL,
+  `Telefone` varchar(15) NOT NULL,
+  `TelefoneO` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`IDContato`),
+  KEY `ID_Cliente` (`ID_Cliente`),
+  CONSTRAINT `Contato_ibfk_1` FOREIGN KEY (`ID_Cliente`) REFERENCES `Cliente` (`IDCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Contato`
+--
+
+LOCK TABLES `Contato` WRITE;
+/*!40000 ALTER TABLE `Contato` DISABLE KEYS */;
+INSERT INTO `Contato` VALUES (1,1,'Rua alguma coisa','João Pessoa','PB',123,'',NULL);
+/*!40000 ALTER TABLE `Contato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Produto`
+--
+
+DROP TABLE IF EXISTS `Produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Produto` (
+  `IDProduto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `preco` float(10,2) NOT NULL,
+  PRIMARY KEY (`IDProduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Produto`
+--
+
+LOCK TABLES `Produto` WRITE;
+/*!40000 ALTER TABLE `Produto` DISABLE KEYS */;
+INSERT INTO `Produto` VALUES (1,'Rexona','Rexona Men active dry 48h',10.50);
+/*!40000 ALTER TABLE `Produto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-15 12:42:35
